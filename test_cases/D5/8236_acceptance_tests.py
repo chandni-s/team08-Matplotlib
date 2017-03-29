93ab3d01e8de81bf1e9f016911bb56f6910fe7de
Jacob Pecile <jacob.pecile@gmail.com>
2017-03-29 12:37:35 -0400

    updated test cases for issue 8236

---
+++ test_cases/D5/8236_acceptance_tests.py
@@ -0,0 +1,134 @@
+'''Acceptance test for issue 8236
+Originally tried to test following test_legend.py method in using image_comparison.
+Did not work as wanted. Need to figure out how to use image_comparison for our
+case. Attempted code has been commented out. Currently, acceptance test needs to be
+run manually by passing in 'show' parameter in main to 'True'.
+'''
+
+
+# from __future__ import (absolute_import, division, print_function,
+#                        unicode_literals)
+# import matplotlib.pylab as plt
+# from matplotlib.testing.decorators import image_comparison
+
+# try:
+#    # mock in python 3.3+
+#    from unittest import mock
+# except ImportError:
+#    import mock
+
+
+def plot_annotations(style, show=False):
+    fig, ax = plt.subplots(1)
+    ax.plot([0, 1], [0, 0], label='line1')
+    ax.plot([0, 1], [1, 1], label='line2')
+    ax.annotate("",
+                xy=(0.4, 1.0),
+                xytext=(0.4, 0.0),
+                arrowprops={'arrowstyle': style, 'color': 'C7'},
+                label='annotation')
+    ax.legend()
+    if show:
+        plt.show()
+
+def plot_annotations_no_style(prop, show=False):
+	fig, ax = plt.subplots(1)
+    ax.plot([0, 1], [0, 0], label='line1')
+    ax.plot([0, 1], [1, 1], label='line2')
+    ax.annotate("",
+                xy=(0.4, 1.0),
+                xytext=(0.4, 0.0),
+                arrowprops={prop: 20, 'color': 'C7'},
+                label='annotation')
+    ax.legend()
+    if show:
+        plt.show()
+
+# @image_comparison(baseline_images=['Dash'], remove_text=True)
+def test_annotation_dash(show=False):
+    plot_annotations('-', show)
+
+
+# @image_comparison(baseline_images=['Right_arrow'], remove_text=True)
+def test_annotation_right_arrow(show=False):
+    plot_annotations('->', show)
+
+
+# @image_comparison(baseline_images=['Right_bracket'], remove_text=True)
+def test_annotation_right_bracket(show=False):
+    plot_annotations('-[', show)
+
+
+# @image_comparison(baseline_images=['Flat_ends'], remove_text=True)
+def test_annotation_flat_ends(show=False):
+    plot_annotations('|-|', show)
+
+
+# @image_comparison(baseline_images=['Right_solid_arrow'], remove_text=True)
+def test_annotation_right_solid_arrow(show=False):
+    plot_annotations('-|>', show)
+
+
+# @image_comparison(baseline_images=['Left_arrow'], remove_text=True)
+def test_annotation_left_arrow(show=False):
+    plot_annotations('<-', show)
+
+
+# @image_comparison(baseline_images=['Both_arrow'], remove_text=True)
+def test_annotation_both_arrow(show=False):
+    plot_annotations('<->', show)
+
+
+# @image_comparison(baseline_images=['Left_solid_arrow'], remove_text=True)
+def test_annotation_left_solid_arrow(show=False):
+    plot_annotations('<|-', show)
+
+
+# @image_comparison(baseline_images=['Both_solid_arrow'], remove_text=True)
+def test_annotation_both_soild_arrow(show=False):
+    plot_annotations('<|-|>', show)
+
+
+# @image_comparison(baseline_images=['Both_solid_arrow'], remove_text=True)
+def test_annotation_fancy(show=False):
+    plot_annotations('fancy', show)
+
+
+# @image_comparison(baseline_images=['Both_solid_arrow'], remove_text=True)
+def test_annotation_simple(show=False):
+    plot_annotations('simple', show)
+
+
+# @image_comparison(baseline_images=['Wedge'])
+def test_annotation_wedge(show=False):
+    plot_annotations('wedge', show)
+
+# @image_comparison(baseline_images=['Width_20'], remove_text=True)
+def test_annotation_width(show=False):
+	plot_annotations_no_style('width', show)
+
+# @image_comparison(baseline_images=['Headwidth_20'], remove_text=True)
+def test_annotation_width(show=False):
+	plot_annotations_no_style('headwidth', show)
+
+# @image_comparison(baseline_images=['Headlength_20'], remove_text=True)
+def test_annotation_width(show=False):
+	plot_annotations_no_style('headlength', show)	
+
+
+if __name__ == '__main__':
+    test_annotation_wedge()
+    test_annotation_fancy()
+    test_annotation_simple()
+    test_annotation_both_soild_arrow()
+    test_annotation_left_solid_arrow()
+    test_annotation_both_arrow()
+    test_annotation_left_arrow()
+    test_annotation_right_solid_arrow()
+    test_annotation_flat_ends()
+    test_annotation_right_bracket()
+    test_annotation_right_arrow()
+    test_annotation_dash()
+    test_annotation_width()
+    test_annotation_headwidth()
+    test_annotation_headlength()
---
+++ test_cases/D5/8236_test.py
@@ -0,0 +1,140 @@
+import unittest
+import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerAnnotation
+from matplotlib.offsetbox import DrawingArea
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import ArrowStyle
+from matplotlib.text import Text
+
+class Test8236(unittest.TestCase):
+
+    # Test if label gets added for a line with arrows (special case) <-->
+    def test_add_legend_arrow_line(self):
+        fig, ax = plt.subplots(1)
+        ax.annotate("",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    arrowprops={'arrowstyle': '<->', 'color': 'C7'},
+                    label='distance')
+        ax.legend()
+
+        handles, labels = ax.get_legend_handles_labels()
+        self.assertTrue('distance' in labels)
+
+    # labels should not have 'distance' as it is initiating with underscore
+    def test_add_legend_underscore(self):
+        fig, ax = plt.subplots(1)
+        ax.annotate("",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    arrowprops={'arrowstyle': '<->', 'color': 'C7'},
+                    label='_distance')
+        ax.legend()
+
+        handles, labels = ax.get_legend_handles_labels()
+        self.assertFalse('distance' in labels)
+
+    # adding integer as legend, should raise TypeError
+    def test_add_legend_integer(self):
+        fig, ax = plt.subplots(1)
+        ax.annotate("",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    arrowprops={'arrowstyle': '<->', 'color': 'C7'},
+                    label='distance')
+        with self.assertRaises(TypeError):
+            ax.legend(1)
+
+    # adding integer as label, should raise TypeError
+    # def test_add_label_integer(self):
+    #     fig, ax = plt.subplots(1)
+    #     ax.annotate("",
+    #                     xy=(0.4, 1.0),
+    #                     xytext=(0.4, 0.0),
+    #                     arrowprops={'arrowstyle': '<->', 'color': 'C7'},
+    #                     label=1)
+
+    #     with self.assertRaises(TypeError):
+    #         ax.legend()
+
+    # only annotation has legend - should have no user warning
+    def test_annotation_only(self):
+        fig, ax = plt.subplots(1)
+        ax.plot([1, 2, 3])
+        ax.annotate("",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    arrowprops={'arrowstyle': '<->', 'color': 'C7'},
+                    label='distance')
+        ax.legend()
+        handles, labels = ax.get_legend_handles_labels()
+        self.assertIn('distance', labels)
+
+    #test the newly created handler with a full annotation
+    def test_handler_full_annotation(self):
+        fig, ax = plt.subplots(1)
+        anno = ax.annotate("test",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    arrowprops={'arrowstyle': '<->', 'color': 'C7'},
+                    label='distance')
+        legend = ax.legend()
+        draw = DrawingArea(20.0,7.0, 0.0, 0.0)
+        handle = HandlerAnnotation()
+        artists = handle.create_artists(legend,anno,0.0,0.0,20.0,7.0, 10.0, draw)
+        self.assertEqual(len(artists), 2)
+        self.assertTrue(isinstance(artists[0],FancyArrowPatch))
+        self.assertTrue(isinstance(artists[1], Text))
+        self.assertTrue(isinstance(artists[0]._arrow_transmuter, ArrowStyle.CurveAB))
+        self.assertEqual(artists[1].get_text(), "Aa")
+
+    # test the newly created handler with an annotation with only text
+    def test_handler_full_annotation(self):
+        fig, ax = plt.subplots(1)
+        anno = ax.annotate("test",
+                           xy=(0.4, 1.0),
+                           xytext=(0.4, 0.0),
+                           label='distance')
+        legend = ax.legend()
+        draw = DrawingArea(20.0, 7.0, 0.0, 0.0)
+        handle = HandlerAnnotation()
+        artists = handle.create_artists(legend, anno, 0.0, 0.0, 20.0, 7.0, 10.0, draw)
+        self.assertEqual(len(artists), 1)
+        self.assertTrue(isinstance(artists[0], Text))
+        self.assertEqual(artists[0].get_text(), "Aa")
+
+    #test the newly created handler with an annotation with only an arrow
+    def test_handler_full_annotation(self):
+        fig, ax = plt.subplots(1)
+        anno = ax.annotate("",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    arrowprops={'arrowstyle': '->', 'color': 'C7'},
+                    label='distance')
+        legend = ax.legend()
+        draw = DrawingArea(20.0,7.0, 0.0, 0.0)
+        handle = HandlerAnnotation()
+        artists = handle.create_artists(legend,anno,0.0,0.0,20.0,7.0, 10.0, draw)
+        self.assertEqual(len(artists), 2)
+        self.assertTrue(isinstance(artists[0],FancyArrowPatch))
+        self.assertTrue(isinstance(artists[1], Text))
+        self.assertTrue(isinstance(artists[0]._arrow_transmuter, ArrowStyle.CurveB))
+        self.assertEqual(artists[1].get_text(), "")
+
+    #test the newly created handler with neither text or an arrow
+    def test_handler_full_annotation(self):
+        fig, ax = plt.subplots(1)
+        anno = ax.annotate("",
+                    xy=(0.4, 1.0),
+                    xytext=(0.4, 0.0),
+                    label='distance')
+        legend = ax.legend()
+        draw = DrawingArea(20.0,7.0, 0.0, 0.0)
+        handle = HandlerAnnotation()
+        artists = handle.create_artists(legend,anno,0.0,0.0,20.0,7.0, 10.0, draw)
+        self.assertEqual(len(artists), 1)
+        self.assertTrue(isinstance(artists[0], Text))
+        self.assertEqual(artists[0].get_text(), "")
+
+if __name__ == '__main__':
+    unittest.main()
